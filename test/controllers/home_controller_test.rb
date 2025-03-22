48e42b3cee47e951147cc0d8000dcb95db2b3853
Mark Cooper <mark.c.cooper@outlook.com>
2025-03-21 20:33:15 -0700

    Setup test login

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should get index when authenticated" do
+    sign_in(users(:admin))
+    get home_index_url
+    assert_response :success
+    # TODO: add assertions about content/behavior
+    # assert_select "h1", "Welcome to Data Toolkit"
+  end
+
+  test "should redirect to login when not authenticated" do
     get home_index_url
     assert_response :redirect
+    assert_redirected_to new_session_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,11 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    def sign_in(user)
+      post session_url, params: {
+        email_address: user.email_address,
+        password: "password"
+      }
+    end
   end
 end
