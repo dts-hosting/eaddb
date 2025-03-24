5a47ac73277fef1e350993b24842da7e35acc753
Mark Cooper <mark.c.cooper@outlook.com>
2025-03-24 16:24:57 -0700

    Check repository is in repo cfg

--- lib/arc_light_exporter.rb
+++ lib/arc_light_exporter.rb
@@ -21,32 +21,50 @@ class ArcLightExporter
 
   def export
     arclight_dir = Gem::Specification.find_by_name("arclight").gem_dir
-    indexer_config = File.join(arclight_dir, "lib", "arclight", "traject", "ead2_config.rb")
+    indexer_cfg = File.join(arclight_dir, "lib", "arclight", "traject", "ead2_config.rb")
+    repositories = YAML.safe_load(destination.config.download)
 
-    destination.transfers.where.not(status: "succeeded").find_each do |transfer|
-      if transfer.record.ead_identifier.blank?
-        error_message = "Record #{transfer.record.id} has no EAD ID"
-        Rails.logger.error(error_message)
-        transfer.failed!(error_message)
-        next
-      end
+    unless repositories.key?(destination.identifier)
+      error_message = "Repository #{destination.identifier} not found in #{repositories.keys}"
+      Rails.logger.error(error_message)
+      return
+    end
 
-      destination.config.open do |repositories|
-        transfer.record.ead_xml.open do |xml|
-          output = `#{command(indexer_config, repositories.path, xml.path)}`
-          if $?.success?
-            transfer.succeeded!
-          else
-            error_message = "Failed to process transfer #{$?.exitstatus}: #{output}"
-            Rails.logger.error(error_message)
-            transfer.failed!(error_message)
-          end
-        end
+    repositories_cfg = Tempfile.new(["destination_", destination.id.to_s, ".yml"])
+    begin
+      repositories_cfg.write(repositories.to_yaml)
+      repositories_cfg.flush
+
+      destination.transfers.where.not(status: "succeeded").find_each do |transfer|
+        process_transfer(transfer, indexer_cfg, repositories_cfg.path)
       end
-    rescue => e
-      error_message = "Failed to process transfer #{transfer.id}: #{e.message}"
+    ensure
+      repositories_cfg.close
+      repositories_cfg.unlink
+    end
+  end
+
+  def process_transfer(transfer, indexer_cfg, repositories_cfg)
+    if transfer.record.ead_identifier.blank?
+      error_message = "Record #{transfer.record.id} has no EAD ID"
       Rails.logger.error(error_message)
       transfer.failed!(error_message)
+      return
     end
+
+    transfer.record.ead_xml.open do |xml|
+      output = `#{command(indexer_cfg, repositories_cfg, xml.path)}`
+      if $?.success?
+        transfer.succeeded!
+      else
+        error_message = "Failed to process transfer #{$?.exitstatus}: #{output}"
+        Rails.logger.error(error_message)
+        transfer.failed!(error_message)
+      end
+    end
+  rescue => e
+    error_message = "Failed to process transfer #{transfer.id}: #{e.message}"
+    Rails.logger.error(error_message)
+    transfer.failed!(error_message)
   end
 end
