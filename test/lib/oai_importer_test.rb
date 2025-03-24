e5a9c616d4f685a8920bebbf7f494fca68b21811
Mark Cooper <mark.c.cooper@outlook.com>
2025-03-24 12:41:45 -0700

    Use table tests for oai import

--- test/lib/oai_importer_test.rb
+++ test/lib/oai_importer_test.rb
@@ -6,6 +6,118 @@ class OaiImporterTest < ActiveSupport::TestCase
     @importer = OaiImporter.new(create_source)
   end
 
+  {
+    extract_eadid: {
+      method_name: :extract_eadid,
+      cases: [
+        {
+          name: "returns value from eadheader/eadid element",
+          xml: <<~XML,
+            <metadata>
+              <ead xmlns="urn:isbn:1-931666-22-9">
+                <eadheader>
+                  <eadid>MS-123</eadid>
+                </eadheader>
+                <archdesc level="collection">
+                  <did><unitid>ABC123</unitid></did>
+                </archdesc>
+              </ead>
+            </metadata>
+          XML
+          expected: "MS-123"
+        },
+        {
+          name: "returns nil when eadid element doesn't exist",
+          xml: <<~XML,
+            <metadata>
+              <ead xmlns="urn:isbn:1-931666-22-9">
+                <eadheader>
+                  <!-- No eadid element -->
+                </eadheader>
+              </ead>
+            </metadata>
+          XML
+          expected: nil
+        },
+        {
+          name: "returns nil for nil input",
+          xml: nil,
+          expected: nil
+        }
+      ]
+    },
+    extract_repository_name: {
+      method_name: :extract_repository_name,
+      cases: [
+        {
+          name: "extracts repository name from namespaced XML",
+          xml: <<~XML,
+            <metadata>
+              <ead xmlns="urn:isbn:1-931666-22-9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                <archdesc level="collection">
+                  <did>
+                    <repository>
+                      <corpname>Allen Doe Research Center</corpname>
+                    </repository>
+                  </did>
+                </archdesc>
+              </ead>
+            </metadata>
+          XML
+          expected: "Allen Doe Research Center"
+        },
+        {
+          name: "returns nil when path doesn't exist",
+          xml: <<~XML,
+            <metadata>
+              <ead>
+                <different>
+                  <structure>Test</structure>
+                </different>
+              </ead>
+            </metadata>
+          XML
+          expected: nil
+        },
+        {
+          name: "returns nil for nil input",
+          xml: nil,
+          expected: nil
+        }
+      ]
+    }
+  }.each do |test_group_name, test_config|
+    test_config[:cases].each do |test_case|
+      test "#{test_config[:method_name]} #{test_case[:name]}" do
+        xml = test_case[:xml].nil? ? nil : REXML::Document.new(test_case[:xml]).root
+        result = @importer.send(test_config[:method_name], xml)
+
+        if test_case[:expected].nil?
+          assert_nil result
+        else
+          assert_equal test_case[:expected], result
+        end
+      end
+    end
+  end
+
+  test "parse_identifier extracts repository path" do
+    [
+      {input: "oai:archivesspace:/repositories/4/resources/1", expected: "/repositories/4"},
+      {input: "oai:archivesspace:/repositories/12/resources/42", expected: "/repositories/12"},
+      {input: nil, expected: nil},
+      {input: "invalid:identifier", expected: nil}
+    ].each do |test_case|
+      result = @importer.parse_identifier(test_case[:input])
+
+      if test_case[:expected].nil?
+        assert_nil result
+      else
+        assert_equal test_case[:expected], result
+      end
+    end
+  end
+
   test "extract_ead returns ead element from metadata" do
     xml = REXML::Document.new(<<~XML).root
       <metadata>
@@ -65,48 +177,5 @@ class OaiImporterTest < ActiveSupport::TestCase
 
   test "extract_ead returns nil for nil input" do
     assert_nil @importer.extract_ead(nil)
-  end
-
-  test "extracts repository name from namespaced XML" do
-    xml = REXML::Document.new(<<~XML).root
-      <metadata>
-        <ead xmlns="urn:isbn:1-931666-22-9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <archdesc level="collection">
-            <did>
-              <repository>
-                <corpname>Allen Doe Research Center</corpname>
-              </repository>
-            </did>
-          </archdesc>
-        </ead>
-      </metadata>
-    XML
-
-    assert_equal "Allen Doe Research Center", @importer.extract_repository_name(xml)
-  end
-
-  test "extract_repository_name returns nil for nil input" do
-    assert_nil @importer.extract_repository_name(nil)
-  end
-
-  test "extract_repository_name returns nil when path doesn't exist" do
-    xml = REXML::Document.new(<<~XML).root
-      <metadata>
-        <ead>
-          <different>
-            <structure>Test</structure>
-          </different>
-        </ead>
-      </metadata>
-    XML
-
-    assert_nil @importer.extract_repository_name(xml)
-  end
-
-  test "parse_identifier extracts repository path" do
-    assert_equal "/repositories/4", @importer.parse_identifier("oai:archivesspace:/repositories/4/resources/1")
-    assert_equal "/repositories/12", @importer.parse_identifier("oai:archivesspace:/repositories/12/resources/42")
-    assert_nil @importer.parse_identifier(nil)
-    assert_nil @importer.parse_identifier("invalid:identifier")
   end
 end
