47f53ed81575bf9e3b989ffe66ac9fdfb818013e
Mark Cooper <mark.c.cooper@outlook.com>
2025-04-14 12:36:07 -0700

    Wrap external links in check (make brakeman happy)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def safe_external_link(url, link_text = nil)
+    link_text ||= url
+    if url.to_s.match?(/\A(http|https):\/\//)
+      link_to(link_text, url, target: "_blank", rel: "noopener noreferrer")
+    else
+      url
+    end
+  end
 end
