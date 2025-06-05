b017dd6abf40755c4204dffeaae9f071aaa7fe06
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-06-05 11:44:07 -0700

    Initial take on session expiration

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,7 @@
 class Session < ApplicationRecord
   belongs_to :user
+
+  def self.sweep(time = 2.hours)
+    where(updated_at: ...time.ago).or(where(created_at: ...1.day.ago)).delete_all
+  end
 end
